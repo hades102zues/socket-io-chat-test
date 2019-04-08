41d7da0c51f903ce8c79bbf278dc3ed8dae1f26c
Joshua Wiggins <joshua_kar@hotmail.com>
2019-04-08 11:41:58 -0400

    added color

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,4 +1,5 @@
 const socket = io();
+const username = "User" + Math.random().toString();
 
 const inputEle = document.querySelector(".message-input");
 const formEle = document.querySelector(".send-spot");
@@ -6,7 +7,7 @@ const chatBox = document.querySelector(".chat-box");
 
 const formSubmitHandler = e => {
 	e.preventDefault();
-	socket.emit("chat message", inputEle.value);
+	socket.emit("chat message", { msg: inputEle.value, username: username });
 	inputEle.value = "";
 };
 
@@ -15,7 +16,7 @@ const scrollToBottom = () => {
 		clientHeight is the amount of the container the user sees
 	**/
 	/**
-		scrollTop is the height at which to put the scroll position
+		scrollTop is the height at which the scroll position is
 	**/
 
 	/**
@@ -27,19 +28,23 @@ const scrollToBottom = () => {
 formEle.addEventListener("submit", formSubmitHandler);
 
 socket.on("chat message", msg => {
+	const lease = 50;
 	let shouldScroll =
-		chatBox.scrollTop >= chatBox.scrollHeight - chatBox.clientHeight - 50;
+		chatBox.scrollTop >=
+		chatBox.scrollHeight - chatBox.clientHeight - lease;
 	//create new h tag with value of name
 	const h5 = document.createElement("h5");
 	h5.className = "username";
-	const h5Text = document.createTextNode("Name");
+	const h5Text = document.createTextNode(msg.username);
 	h5.appendChild(h5Text);
+
+	if (msg.username !== username) h5.style.color = "purple";
 
 	//create a new p tag with value message
 	const p = document.createElement("p");
 	p.className = "message-text";
 
-	const pText = document.createTextNode(msg);
+	const pText = document.createTextNode(msg.msg);
 	p.appendChild(pText);
 
 	//create a new div with class message box
@@ -54,7 +59,7 @@ socket.on("chat message", msg => {
 	chatBox.appendChild(message);
 
 	if (shouldScroll) scrollToBottom();
-	console.log(chatBox.scrollTop);
+	console.log(h5);
 });
 
 // const chatBoxScrollHandler = () => {
